697b1b8a4abd97a5eb826a3a43fe8566baf14d1c
Laura Isadora <laura.moreira17@hotmail.com>
2025-06-17 20:47:15 +0000

    Adiciona posicionamento de navios diagonais no tabuleiro de Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,62 +1,51 @@
 #include <stdio.h>
 
-// Tamanho fixo do tabuleiro
-#define TAMANHO 10
-
-// Valor para representar parte do navio
-#define NAVIO 3
+#define TAM 10
+#define TAM_NAVIO 3
 
 int main() {
-    // matriz do tabuleiro
-    int tabuleiro[TAMANHO][TAMANHO];
-
-    for (int i = 0; i < TAMANHO; i++) {
-        for (int j = 0; j < TAMANHO; j++) {
-            tabuleiro[i][j] = 0;
-        }
-    }
-
-    // Tamanho fixo dos navios
-    int tamanho_navio = 3;
+    int tabuleiro[TAM][TAM] = {0}; // Inicializa todas as posições com água (0)
 
     // Coordenadas dos navios
-    int linha_horizontal = 2, coluna_horizontal = 1; // navio horizontal começando em (2,1)
-    int linha_vertical = 5, coluna_vertical = 6;     // navio vertical começando em (5,6)
+    int navio1_linha = 1, navio1_coluna = 2; // Horizontal
+    int navio2_linha = 5, navio2_coluna = 7; // Vertical
+    int navio3_linha = 0, navio3_coluna = 0; // Diagonal principal (↘)
+    int navio4_linha = 0, navio4_coluna = 9; // Diagonal secundária (↙)
 
-    if (coluna_horizontal + tamanho_navio <= TAMANHO) {
-        int pode_posicionar = 1;
-        for (int i = 0; i < tamanho_navio; i++) {
-            if (tabuleiro[linha_horizontal][coluna_horizontal + i] == NAVIO) {
-                pode_posicionar = 0;
-                break;
-            }
-        }
-        if (pode_posicionar) {
-            for (int i = 0; i < tamanho_navio; i++) {
-                tabuleiro[linha_horizontal][coluna_horizontal + i] = NAVIO;
-            }
+    // Validação e posicionamento do navio horizontal
+    if (navio1_coluna + TAM_NAVIO <= TAM) {
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            tabuleiro[navio1_linha][navio1_coluna + i] = 3;
         }
     }
 
-    if (linha_vertical + tamanho_navio <= TAMANHO) {
-        int pode_posicionar = 1;
-        for (int i = 0; i < tamanho_navio; i++) {
-            if (tabuleiro[linha_vertical + i][coluna_vertical] == NAVIO) {
-                pode_posicionar = 0;
-                break;
-            }
-        }
-        if (pode_posicionar) {
-            for (int i = 0; i < tamanho_navio; i++) {
-                tabuleiro[linha_vertical + i][coluna_vertical] = NAVIO;
-            }
+    // Validação e posicionamento do navio vertical
+    if (navio2_linha + TAM_NAVIO <= TAM) {
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            tabuleiro[navio2_linha + i][navio2_coluna] = 3;
         }
     }
 
-    // Impressão do tabuleiro no console
-    printf("Tabuleiro Batalha Naval (0 = água, 3 = navio):\n\n");
-    for (int i = 0; i < TAMANHO; i++) {
-        for (int j = 0; j < TAMANHO; j++) {
+    // Validação e posicionamento do navio diagonal principal
+    if (navio3_linha + TAM_NAVIO <= TAM && navio3_coluna + TAM_NAVIO <= TAM) {
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            if (tabuleiro[navio3_linha + i][navio3_coluna + i] == 0)
+                tabuleiro[navio3_linha + i][navio3_coluna + i] = 3;
+        }
+    }
+
+    // Validação e posicionamento do navio diagonal secundária
+    if (navio4_linha + TAM_NAVIO <= TAM && navio4_coluna - (TAM_NAVIO - 1) >= 0) {
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            if (tabuleiro[navio4_linha + i][navio4_coluna - i] == 0)
+                tabuleiro[navio4_linha + i][navio4_coluna - i] = 3;
+        }
+    }
+
+    // Exibição do tabuleiro
+    printf("Tabuleiro Batalha Naval:\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
